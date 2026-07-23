29830605988bf11244498a30df3cbe42775dbccd
Datym1984 <ivanmatveev980@gmail.com>
2026-07-23 21:37:42 +0500

    Update suffix array.cpp

--- strings/suffix array.cpp
+++ strings/suffix array.cpp
@@ -1,32 +1,33 @@
-vector<int> SuffixArray(string s) {
-    int n = s.size();
-    vector<int> P(n), C(n);
-    for (int i = 0; i < n; i++)
-        P[i] = i;
-    sort(P.begin(), P.end(), [&](int i, int j){return s[i] < s[j];});
-    int classes = 0;
-    for (int i = 1; i < n; i++) {
-        if (s[P[i - 1]] != s[P[i]])
-            classes++;
-        C[P[i]] = classes;
-    }
+vector<int> suffix_array(string &s) {
+    s += '#';
+    int n = (int) s.size();
+    vector<int> P(n);
+    iota(all(P), 0);
+    sort(all(P), [&s](int i, int j){return s[i] < s[j];});
+    vector<int> C(n);
+    for (int i = 1; i < n; i++)
+        C[P[i]] = C[P[i - 1]] + (s[P[i]] != s[P[i - 1]]);
     for (int h = 0; (1 << h) < n; h++) {
-        sort(all(P), [&](auto i, auto j){
-            pair<int, int> i2 = {C[i], C[(i + (1 << h)) % n]};
-            pair<int, int> j2 = {C[j], C[(j + (1 << h)) % n]};
-            return i2 < j2;
-        });
-        C[P[0]] = 0;
-        classes = 0;
+        vector<vector<int>> cnt(n);
+        for (int i = 0; i < n; i++) {
+            P[i] = (P[i] - (1 << h) + n) % n;
+            cnt[C[P[i]]].push_back(P[i]);
+        }
+        vector<int> P2;
+        for (int i = 0; i < n; i++) {
+            for (int j : cnt[i])
+                P2.push_back(j);
+        }
         vector<int> C2(n);
         for (int i = 1; i < n; i++) {
-            int mid1 = (P[i - 1] + (1 << h)) % n;
-            int mid2 = (P[i] + (1 << h)) % n;
-            if (C[mid1] != C[mid2] || C[P[i - 1]] != C[P[i]])
-                classes++;
-            C2[P[i]] = classes;
+            auto k1 = mp(C[P2[i - 1]], C[(P2[i - 1] + (1 << h)) % n]);
+            auto k2 = mp(C[P2[i]], C[(P2[i] + (1 << h)) % n]);
+            C2[P2[i]] = C2[P2[i - 1]] + (k1 != k2);
         }
-        C = C2;
+        swap(C, C2);
+        swap(P, P2);
     }
+    P.erase(P.begin());
+    s.pop_back();
     return P;
 }
