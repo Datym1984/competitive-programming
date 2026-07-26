159527191023cd0ca73031f3f74d5611032f387b
Datym1984 <ivanmatveev980@gmail.com>
2026-07-26 22:46:33 +0500

    Update is_point_in_polygon.cpp

--- geometry/is_point_in_polygon.cpp
+++ geometry/is_point_in_polygon.cpp
@@ -1,6 +1,6 @@
 bool in_polygon(vector<pt> &P, pt p) {
     int n = P.size();
-    if (cross(P[1] - P[0], p - P[0]) < 0 || cross(P[0] - P[n - 1], p - P[n - 1]) < 0)// <= 0 - if pt cant be in polygon
+    if (cross(P[1] - P[0], p - P[0]) < 0 || cross(P[0] - P[n - 1], p - P[n - 1]) < 0)// <= 0 - if pt cant be on side
         return false;
     int l = 1, r = n - 1;
     while (r - l > 1) {
@@ -10,5 +10,5 @@ bool in_polygon(vector<pt> &P, pt p) {
         else
             r = m;
     }
-    return cross(P[r] - P[l], p - P[l]) >= 0;// > 0 - if pt cant be in polygon
+    return cross(P[r] - P[l], p - P[l]) >= 0;// > 0 - if pt cant be on side
 }
