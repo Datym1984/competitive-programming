d7abac328997c4352f582606432902f5571bbd80
Datym1984 <ivanmatveev980@gmail.com>
2026-09-20 11:37:54 +0500

    Update ModInt.cpp

--- number_theory/ModInt.cpp
+++ number_theory/ModInt.cpp
@@ -4,63 +4,66 @@ struct ModInt {
 
     ModInt(int val=0) : val(val) {}
 
-    operator int() {
+    explicit operator int() {
         return val;
     }
 
-    friend istream& operator>>(istream &i, ModInt &x) {
-        return i >> x.val;
+    friend istream& operator>>(istream& i, ModInt &rhs) {
+        return i >> rhs.val;
     }
 
-    friend ostream& operator<<(ostream &o, const ModInt &x) {
-        return o << x.val;
+    friend ostream& operator<<(ostream& o, const ModInt &rhs) {
+        return o << rhs.val;
     }
 
-    friend ModInt operator+(const ModInt &lhs, const ModInt &rhs) {
-        return ModInt((lhs.val + rhs.val) % mod);
+    ModInt& operator+=(const ModInt& rhs) {
+        val += rhs.val;
+        val -= (val >= mod) * mod;
+        return *this;
     }
 
-    friend ModInt& operator+=(ModInt &lhs, const ModInt &rhs) {
-        lhs.val = (lhs.val + rhs.val) % mod;
-        return lhs;
+    ModInt operator+(const ModInt& rhs) const {
+        return ModInt(*this) += rhs;
     }
 
-    friend ModInt operator-(const ModInt &lhs, const ModInt &rhs) {
-        return ModInt(((lhs.val - rhs.val) % mod + mod) % mod);
+    ModInt& operator-=(const ModInt& rhs) {
+        val -= rhs.val;
+        val += (val < 0) * mod;
+        return *this;
     }
 
-    friend ModInt& operator-=(ModInt &lhs, const ModInt &rhs) {
-        lhs.val = ((lhs.val - rhs.val) % mod + mod) % mod;
-        return lhs;
+    ModInt operator-(const ModInt& rhs) const {
+        return ModInt(*this) -= rhs;
     }
 
-    friend ModInt operator*(const ModInt &lhs, const ModInt &rhs) {
-        return ModInt(((ll) lhs.val * rhs.val) % mod);
+    ModInt& operator*=(const ModInt& rhs) {
+        val = (ll) val * rhs.val % mod;
+        return *this;
     }
 
-    friend ModInt& operator*=(ModInt &lhs, const ModInt &rhs) {
-        lhs.val = ((ll) lhs.val * rhs.val) % mod;
-        return lhs;
+    ModInt operator*(const ModInt& rhs) const {
+        return ModInt(*this) *= rhs;
     }
 
-    friend ModInt operator/(const ModInt &lhs, const ModInt &rhs) {
-        return lhs * rhs.power(mod - 2);
+    ModInt operator/=(const ModInt& rhs) {
+        *this *= rhs.power(mod - 2);
+        return *this;
     }
 
-    friend ModInt& operator/=(ModInt &lhs, const ModInt &rhs) {
-        lhs = lhs / rhs;
-        return lhs;
+    ModInt operator/(const ModInt& rhs) const {
+        return ModInt(*this) /= rhs;
     }
 
 private:
     ModInt power(int ind) const {
-        if (ind == 0)
-            return ModInt(1);
-        else if (ind % 2 == 0) {
-            ModInt res = power(ind / 2);
-            return res * res;
+        ModInt res = 1;
+        ModInt p = *this;
+        while (ind) {
+            if (ind & 1)
+                res *= p;
+            p *= p;
+            ind >>= 1;
         }
-        else
-            return this->power(ind - 1) * (*this);
+        return res;
     }
 };
