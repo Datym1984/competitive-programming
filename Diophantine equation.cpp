f64ea77aa45a1d9a41e4b6057872da7bb2489fa8
Datym1984 <ivanmatveev980@gmail.com>
2026-09-22 11:07:40 +0500

    Update Diophantine equation.cpp

--- Diophantine equation.cpp
+++ Diophantine equation.cpp
@@ -16,18 +16,10 @@ pair<ll, ll> solve_eq(int a, int b, int c) {
     int x = 0, y = 0;
     int g = expanded_gcd(abs(a), abs(b), x, y);
     if (abs(c) % g != 0)
-        return mp(-1, -1);
+        return mp(0, 0);
     if (a < 0) x = -x;
     if (b < 0) y = -y;
     x *= c / g;
     y *= c / g;
-
-    if (x < 0) {
-        int cnt = (abs(x) + b - 1) / b;
-        return mp(x + (ll) cnt * b, y - (ll) cnt * a);
-    }
-    if (x >= 0) {
-        int cnt = x / b;
-        return mp(x - (ll) cnt * b, y + (ll) cnt * a);
-    }
+    return mp((ll) x * (c / g), (ll) y * (c / g));
 }
